79d9e9b7d17aafed436ea9f246a2964a853cb412
rifqirizaldi <97571652+rifqirizaldi@users.noreply.github.com>
2024-05-01 06:47:51 +0700

    Create mocharc.cjs

---
+++ report-portal/mocharc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    reporter: "@reportportal/agent-js-mocha",
+    'reporter-option': [
+        'endpoint=https://demo.reportportal.io/api/v1',
+        'apiKey=Testing-mocha_T06sVOJuR_C62fGHQKt-2qyO8YqAVjOA5vGDZKErK1V-wOeyzxz5DJScR3sYgCrC',
+        'project=rifqirizaldi_personal',
+        'launch=Script Automation Api Testing Dari Mocha',
+    ]
+}
