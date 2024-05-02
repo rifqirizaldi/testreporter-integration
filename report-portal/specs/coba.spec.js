b59b4aeab5c0d01a83f9205c67cd80079e6fc9f9
rifqirizaldi <97571652+rifqirizaldi@users.noreply.github.com>
2024-05-02 13:55:55 +0700

    Update coba.spec.js
    Merubah isi coba.spec.js ke tugas testing api

--- report-portal/specs/coba.spec.js
+++ report-portal/specs/coba.spec.js
@@ -1,19 +1,67 @@
-import {expect} from 'chai'
-import { describe, it } from 'mocha'
+import * as chai from 'chai'
+import schema from '../schema/post_api.js'
+import chaiJsonSchemaAjv from 'chai-json-schema-ajv'
+import supertest from 'supertest'
+import { describe } from 'mocha'
 
-describe("C001 - Coba test reporter",function(){
-    it("F001 - Test sum", function(){
-        let sum = 1+1
-        expect(sum).to.equal(2)
+chai.use(chaiJsonSchemaAjv)
+
+const {expect} = chai
+const request = supertest('https://dummyjson.com')
+
+describe("Test API", ()=>{
+    it("GET ID data number 1", async function () {
+       try {
+        let res = await request.get("/posts/1")
+        expect(res.body).to.be.jsonSchema(schema)
+        console.log("GET Data Success",res.body)
+       } catch (error) {
+        console.log(error)
+       }
+    
     })
 
-    it("F002 - Test type data", function(){
-        let word = "ARIS"
-        expect(word).to.be.a('string')
+    it("POST New data", async()=>{
+        try {
+            let res = await request.post("/posts/add")
+            .send({
+                title: 'His mother and family',
+                body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
+                userId: 9,
+                tags: [ 'history', 'american', 'crime' ],
+                reactions: 10
+            })
+            expect(res.body).to.be.jsonSchema(schema)
+            console.log("POST Data Success", res.body)
+        } catch (error) {
+            console.log(error)
+        }
     })
 
-    it("F003 - Test type data", function(){
-        let word = 123
-        expect(word).to.be.a('boolean')
+    it("PUT Data with ID number 1", async()=>{
+        try {
+            let res = await request.post("/posts/1")
+            .send({
+                title: 'His mother and family',
+                body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
+                userId: 10,
+                tags: [ 'social', 'mexico', 'wholesome' ],
+                reactions: 100
+            })
+            expect(res.body).to.be.jsonSchema(schema)
+            console.log("PUT Data Success", res.body)
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    it("Delete Data with ID number 1", async()=>{
+        try {
+            let res = await request.delete('/posts/1')
+            expect(res.body).to.be.jsonSchema(schema)
+            console.log("DELETE Data Success")
+        } catch (error) {
+            console.log(error)
+        }
     })
 })
